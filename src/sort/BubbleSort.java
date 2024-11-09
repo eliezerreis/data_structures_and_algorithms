6c6fe41ac8e66114bf3acf1382f9ef1c150a35ca
Eliezer Oliveira <eliezerreis@snews.tv>
2024-11-08 19:51:30 -0500

    Add some edge cases for the bubble sort

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public int[] sort(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return new int[]{};
+
+        if(nums.length == 1)
+            return nums;
 
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums.length - i; j++) {
@@ -21,5 +26,9 @@ public class BubbleSort {
     public static void main(String[] args) {
         BubbleSort bubble = new BubbleSort();
         System.out.println(Arrays.toString(bubble.sort(new int[]{10, -10, 25, 7, 8, 12, -5, 9, -15})));
+        System.out.println(Arrays.toString(bubble.sort(new int[]{9,9,9,9})));
+        System.out.println(Arrays.toString(bubble.sort(null)));
+        System.out.println(Arrays.toString(bubble.sort(new int[]{})));
+        System.out.println(Arrays.toString(bubble.sort(new int[]{1})));
     }
 }
