9e300f4041c885ed99b9c525011357b880cbcb7c
Eliezer Oliveira <eliezerreis@snews.tv>
2024-11-11 14:58:01 -0500

    Refactor the hashtable

--- src/hashtables/hashtable/ProbingHashtable.java
+++ src/hashtables/hashtable/ProbingHashtable.java
@@ -2,46 +2,6 @@ package hashtables.hashtable;
 
 import entities.Employee;
 
-class Store {
-    private final int key; // This save the hashed key used to save the employee
-    private Employee employee;
-
-    private boolean deleted;
-
-    public Store(int key, Employee employee) {
-        this.key = key;
-        this.employee = employee;
-        this.deleted = false;
-    }
-
-    @Override
-    public String toString() {
-        return "Store{" + " key=" + key + ", employee=" + employee + (deleted ? ", deleted" : "") + '}';
-    }
-
-    public int getKey() {
-        return key;
-    }
-
-    public Employee getEmployee() {
-        return employee;
-    }
-
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void delete() {
-        this.deleted = true;
-    }
-
-}
-
-
 public class ProbingHashtable {
     Store[] map;
 
---
+++ src/hashtables/hashtable/Store.java
@@ -0,0 +1,43 @@
+package hashtables.hashtable;
+
+import entities.Employee;
+
+public class Store {
+    private final int key; // This save the hashed key used to save the employee
+    private Employee employee;
+
+    private boolean deleted;
+
+    public Store(int key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+        this.deleted = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Store{" + " key=" + key + ", employee=" + employee + (deleted ? ", deleted" : "") + '}';
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void delete() {
+        this.deleted = true;
+    }
+
+}
+
